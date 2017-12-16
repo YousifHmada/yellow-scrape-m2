b6190c24a2721edad4a5a29af060670487f3e578
YousifHmada <1401724@eng.asu.edu.eg>
2017-12-16 17:32:07 +0200

    implementing level1 crawling function

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var fetchAndExecute = require('./child');
 var workerFarm = require('worker-farm')
   , workers    = workerFarm({
     maxCallsPerWorker           : Infinity
@@ -11,40 +12,42 @@ var workerFarm = require('worker-farm')
 }, require.resolve('./child'));
 
 //create a scrape function: 
-var scrape = function(parallel) {
+var scrape = function(parallel, numPages) {
 	return new Promise((resolve, reject)=>{
 		var date = new Date();
-		ret = 0;
-		console.time('operation');
-		let sum = 0;
+		var ret = 0;
+		let sum = [];
 		if(parallel){
-			for (var i = 0; i < 50; i++) {
-			  //generating 50 tasks
-			  workers('#' + i + ' FOO', function (err, out, outp) {
+			for (var i = 1; i <= numPages; i++) {
+			  //generating tasks
+			  workers(i, function (err, out) {
 			    //this is called after the task is finished
-			    // console.log(outp);
-			    sum += out;
-			    if(++ret == 50){
+			    // console.log(out);
+			    sum.push(out);
+			    if(++ret == numPages){
 			    	//this is called after all tasks are finished
 			    	// console.log('processes finished');
-			    	console.timeEnd('operation'); // operation: 1753.916ms
+			    	console.log('parallel operation ', (new Date() - date) + 'ms'); // operation: 17numPages3.916ms
 			    	resolve({sum,time: (new Date() - date) + 'ms'});
 			    }
 			  })
 			}//parallel
 		}else{
 			//simulate sequential mode
-			for (var j = 0; j < 50; j++) {
-			  let out = 0;
-			  for (var i = 0; i < 10000000; i++) {
-			  	out += i;
-			  }
-			  sum += out;
-			  // console.log(j ,out);
-			}
-			console.timeEnd('operation'); // operation: 2514.907ms
-			resolve({sum,time: (new Date() - date) + 'ms'});
-			//sequential
+			for (var i = 1; i <= numPages; i++) {
+			  //generating tasks
+			  fetchAndExecute(i, function (err, out) {
+			  	//this is called after the task is finished
+			    // console.log(out);
+			    sum.push(out);
+			    if(++ret == numPages){
+			    	//this is called after all tasks are finished
+			    	// console.log('processes finished');
+			    	console.log('sequential operation ', (new Date() - date) + 'ms'); // operation: 1753.916ms
+			    	resolve({sum,time: (new Date() - date) + 'ms'});
+			    }
+			  });
+			}//sequential
 		}
 	});
 }
@@ -53,15 +56,17 @@ var scrape = function(parallel) {
 //create a server object: 
 http.createServer(function (req, res) {
 	if(req.url == '/p'){
-		scrape(true)
+		scrape(true, 50)
 			.then((result)=>{
-				res.write(JSON.stringify(result)); //write a response to the client
+				res.setHeader('Content-Type', 'application/json');
+				res.write(JSON.stringify(result, null, 2)); //write a response to the client
   				res.end(); //end the response
 			});
 	}else if(req.url == '/s'){
-		scrape(false)
+		scrape(false, 50)
 			.then((result)=>{
-				res.write(JSON.stringify(result)); //write a response to the client
+				res.setHeader('Content-Type', 'application/json');
+				res.write(JSON.stringify(result, null, 2)); //write a response to the client
   				res.end(); //end the response
 			});
 	}
--- child.js
+++ child.js
@@ -1,7 +1,68 @@
+const cheerio = require('cheerio');
+var axios = require('axios');
 module.exports = function (inp, callback) {
-  let out = 0;
-  for (var i = 0; i < 10000000; i++) {
-  	out += i;
-  }
-  callback(null, out, inp + ' ' + out + ' BAR (' + process.pid + ')')
+  var date = new Date();
+  var $  = '';
+  // console.log('req: ' + inp + ' , ' + process.pid);
+  axios.get('https://www.yellowpages.com.eg/en/category/fast-food/p' + inp)
+  	.then((response)=>{
+  		// console.log(response.data);
+  		$ = cheerio.load(response.data, {
+          withDomLvl1: true,
+          normalizeWhitespace: false,
+          xmlMode: false,
+          decodeEntities: true
+      });
+      var resultsObject = $('.content-widget').not('.searchDetails');
+  		var results = [];
+      for (var i = 0; i < resultsObject.length; i++) {
+        results.push({
+          companyName: resultsObject.eq(i).find('.companyName').text().trim(),
+          address: resultsObject.eq(i).find('.row.address').text()
+                    .replace(/Map it/g, ($item)=>{
+                      return '';
+                    }).trim(),
+          about: resultsObject.eq(i).find('.aboutUs').text()
+                  .replace(/[\n]/g, ($item)=>{
+                    return '';
+                  }).trim(),
+          image: (()=>{
+                    let temp = resultsObject.eq(i).find('.content-image.imageAvailable')
+                      .find('img').attr('src');
+                    if(temp == undefined)return '';
+                    return 'https:' + temp;
+                  })(),
+          keywords: (()=>{
+                    let tempArr = [];
+                    let tempFirst = resultsObject.eq(i).find('.keyword').text().replace(/ */g, ()=>'').trim();
+                    if(tempFirst == '')return tempArr;
+                    tempFirst.split(/\n/).forEach(($item)=>{
+                      tempArr.push($item);
+                    });
+                    tempFirst = resultsObject.eq(i).find('.otherKeywords').data('content');
+                    if(tempFirst == undefined)return tempArr;
+                    tempFirst.trim().split(/<br>/).forEach(($item)=>{
+                      tempArr.push($item);
+                    });
+                    return tempArr;
+                  })(),
+          categories: (()=>{
+                    let tempArr = [];
+                    let tempFirst = resultsObject.eq(i).find('.category').text().replace(/ */g, ()=>'').trim();
+                    if(tempFirst == '')return tempArr;
+                    tempFirst.split(/\n/).forEach(($item)=>{
+                      tempArr.push($item);
+                    });
+                    tempFirst = resultsObject.eq(i).find('.otherCategories').data('content');
+                    if(tempFirst == undefined)return tempArr;
+                    tempFirst.trim().split(/<br>/).forEach(($item)=>{
+                      tempArr.push($item);
+                    });
+                    return tempArr;
+                  })()
+        });
+      }
+      // console.log('response: ' + inp + ' , ' + process.pid + ' ,time: ' + (new Date() - date) + 'ms');
+  		callback(null, results)
+  	});
 }
