a3de96d80ec18c47f1fdf6484bbb06271070c0dd
YousifHmada <1401724@eng.asu.edu.eg>
2017-12-17 13:15:50 +0200

    adding database connection

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	"postgre": {
+	    "user": "gheqtbls",
+		"host": "horton.elephantsql.com",
+	    "database": "gheqtbls",
+	    "password": "itRnq6U6I6Reqt5ltEOlAAinWtCsVKNK",
+	    "port": 5432,
+	    "url": "postgres://gheqtbls:itRnq6U6I6Reqt5ltEOlAAinWtCsVKNK@horton.elephantsql.com:5432/gheqtbls"
+	}
+}
